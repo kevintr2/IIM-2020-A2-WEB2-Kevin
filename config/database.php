32c224b27ad4e367b8584b8547d401ce0114b6d6
Kevin Tran <kevintran@hotmail.de>
2017-02-12 16:23:27 +0100

    controller and migration done

--- config/database.php
+++ config/database.php
@@ -51,6 +51,7 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
+            'unix_socket' => env('MAC_UNIX_SOCKET'),
         ],
 
         'pgsql' => [
